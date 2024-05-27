869c24f7fd1617fcb6ea9f4f397ef7bb478a42c6
Nooka6350 <jamesnziokamutua1@gmail.com>
2024-05-27 20:46:24 +0000

    Configure

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,23 +1,35 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
-    node: true,
+    browser: false,
+    es6: true,
     jest: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
     'airbnb-base',
+    'plugin:jest/all',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['jest'],
   rules: {
-    // Your custom rules can go here
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
